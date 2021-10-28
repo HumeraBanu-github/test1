a09bacc493252788860e4e43d0ee5367ecd7fbda
HumeraBanu-github <humerashaik8050@gmail.com>
2021-10-28 23:45:05 +0530

    first comment

--- hunger.java
+++ hunger.java
@@ -1,6 +1,6 @@
 class hunger
 {
-public static void main (string[] args)
+public static void main (String[] args)
 {
 System.out.println("hello world!");
 System.out.println("Adding another to check the commiting");
